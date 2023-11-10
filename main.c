9f590718bdb64f86ab418301fbf656d25c41096a
Трушкин Илья Андреевич <145765535+blya-eto-chto-za-sueta@users.noreply.github.com>
2023-11-10 22:45:17 +0700

    Update main.c

--- main.c
+++ main.c
@@ -2,45 +2,40 @@
 #include <stdlib.h>
 #include <stdint.h>
 
-int main(){
-    FILE* fin = fopen("input.txt","rb");
+int main() {
+    FILE *fin = fopen("input.txt", "rb");
     freopen("output.txt", "w", stdout);
     int32_t N;// количество байтов в файле
-    fread(&N,sizeof (int32_t),1,fin);
-    int32_t countBites=5;// количество считанных байтов
+    fread(&N, sizeof(int32_t), 1, fin);
+    int32_t countBites = 5;// количество считанных байтов
 
     printf("{\n");
-    while(countBites<N){
+    while (countBites < N) {
         int8_t T;
-        fread(&T,sizeof (int8_t),1,fin);
+        fread(&T, sizeof(int8_t), 1, fin);
         countBites++;
+        // запись и вывод имени поля
+        printf("\t");
+        printf("\"");
+
+        char tempName=1;
+        while (1 == 1) { // проверяем последний записанный байт
+            fread(&tempName, sizeof(char), 1, fin);
+            if (tempName == 0) {
+                countBites++;
+                break;
+            }
+            printf("%c", tempName);
+            countBites++;
+        }
+        printf("\": ");
+
         switch (T) {
             case 1 : {//  значение типа double
-                char *fieldName = (char *) malloc(sizeof(char) * 1000);
-
-                fieldName[0] = 1;// чтобы зайти в изначальный цикл
-                int32_t idx = 0;
-
-                while (1 == 1) { // проверяем последний записанный байт
-                    fread(&fieldName[idx], sizeof(char), 1, fin);
-                    if (fieldName[idx] == 0){
-                        countBites++;
-                        break;
-                    }
-                    countBites++;
-                    idx++;
-                }// посимвольная запись имя поля
-
                 double meaning;
                 fread(&meaning, sizeof(double), 1, fin);
                 countBites += 8;
 
-                printf("\t");
-                printf("\"");
-                for (int i = 0; i < idx; i++) {
-                    printf("%c", fieldName[i]);
-                }
-                printf("\": ");
                 printf("%0.15g", meaning);
 
                 if (countBites < N) {
@@ -49,21 +44,8 @@ int main(){
                 printf("\n");
                 break;
             }
+
             case 2 : {// string
-                char *fieldName = (char *) malloc(sizeof(char) * 1000);
-
-                int32_t idx = 0;
-
-                while (1 == 1) { // проверяем последний записанный байт
-                    fread(&fieldName[idx], sizeof(char), 1, fin);
-                    if (fieldName[idx] == 0) {
-                        countBites++;
-                        break;
-                    }
-                    countBites++;
-                    idx++;
-                }// посимвольная запись имя поля
-
                 int32_t L;// равно количеству байтов в строке, включая дополнительный нулевой байт.
                 fread(&L, sizeof(int32_t), 1, fin);
                 countBites += 4;
@@ -71,13 +53,6 @@ int main(){
                 char *string = (char *) malloc(sizeof(char) * L);
                 fread(string, sizeof(char), L, fin);
                 countBites += L;
-
-                printf("\t");
-                printf("\"");
-                for (int i = 0; i < idx; i++) {
-                    printf("%c", fieldName[i]);
-                }
-                printf("\": ");
 
                 printf("\"");
                 printf("%s", string);
@@ -89,136 +64,53 @@ int main(){
                 break;
             }
             case 8 : { //bool
-                char *fieldName = (char *) malloc(sizeof(char) * 1000);
-
-                fieldName[0] = 1;// чтобы зайти в изначальный цикл
-                int32_t idx = 0;
-
-                while (1 == 1) { // проверяем последний записанный байт
-                    fread(&fieldName[idx], sizeof(char), 1, fin);
-                    if (fieldName[idx] == 0) {
-                        countBites++;
-                        break;
-                    }
-                    countBites++;
-                    idx++;
-                }// посимвольная запись имя поля
-
                 char isBoll;
                 fread(&isBoll, sizeof(char), 1, fin);
                 countBites++;
 
-                printf("\t");
-                printf("\"");
-                for (int i = 0; i < idx; i++) {
-                    printf("%c", fieldName[i]);
-                }
-                printf("\": ");
-
-                if (isBoll==1){
+                if (isBoll == 1) {
                     printf("true");
-                }
-                else{
+                } else {
                     printf("false");
                 }
 
                 if (countBites < N) {
-                   printf(",");
+                    printf(",");
                 }
                 printf("\n");
                 break;
             }
             case 10: {//значение типа null.
-                char* fieldName=(char*)malloc(sizeof (char)*1000);
 
-                fieldName[0]=1;// чтобы зайти в изначальный цикл
-                int32_t idx=0;
-
-                while (1==1){ // проверяем последний записанный байт
-                    fread(&fieldName[idx],sizeof(char),1,fin);
-                    if(fieldName[idx]==0){
-                        countBites++;
-                        break;
-                    }
-                    countBites++;
-                    idx++;
-                }// посимвольная запись имя поля
-
-                printf("\t");
-                printf("\"");
-                for (int i=0;i<idx;i++){
-                    printf("%c",fieldName[i]);
-                }
-                printf("\": ");
                 printf("null");
 
-                if (countBites<N){
+                if (countBites < N) {
                     printf(",");
                 }
                 printf("\n");
                 break;
             }
             case 16: {//значение типа int32
-                char* fieldName=(char*)malloc(sizeof (char)*1000);
-
-                fieldName[0]=1;// чтобы зайти в изначальный цикл
-                int32_t idx=0;
-
-                while (1==1){ // проверяем последний записанный байт
-                    fread(&fieldName[idx],sizeof(char),1,fin);
-                    if(fieldName[idx]==0){
-                        countBites++;
-                        break;
-                    }
-                    countBites++;
-                    idx++;
-                }// посимвольная запись имя поля
                 int32_t meaning;
-                fread(&meaning,sizeof(int32_t),1,fin);
-                countBites+=4;
+                fread(&meaning, sizeof(int32_t), 1, fin);
+                countBites += 4;
 
-                printf("\t");
-                printf("\"");
-                for (int i=0;i<idx;i++){
-                    printf("%c",fieldName[i]);
-                }
-                printf("\": ");
-                printf("%d",meaning);
+                printf("%d", meaning);
 
-                if (countBites<N){
+                if (countBites < N) {
                     printf(",");
                 }
                 printf("\n");
                 break;
             }
             case 18: {//значение типа int64
-                char* fieldName=(char*)malloc(sizeof (char)*1000);
-
-                fieldName[0]=1;// чтобы зайти в изначальный цикл
-                int32_t idx=0;
-
-                while (1==1){ // проверяем последний записанный байт
-                    fread(&fieldName[idx],sizeof(char),1,fin);
-                    if(fieldName[idx]==0){
-                        countBites++;
-                        break;
-                    }
-                    countBites++;
-                    idx++;
-                }// посимвольная запись имя поля
                 int64_t meaning;
-                fread(&meaning,sizeof(int64_t),1,fin);
-                countBites+=8;
+                fread(&meaning, sizeof(int64_t), 1, fin);
+                countBites += 8;
 
-                printf("\t");
-                printf("\"");
-                for (int i=0;i<idx;i++){
-                    printf("%c",fieldName[i]);
-                }
-                printf("\": ");
                 printf("%d",meaning);
 
-                if (countBites<N){
+                if (countBites < N) {
                     printf(",");
                 }
                 printf("\n");
